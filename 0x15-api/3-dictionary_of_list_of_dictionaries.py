3ff72709a3822d5b44bdc5e6a1fe8f8c5d6e22f8
Ige Victor Kolade <victorkoladeige@gmail.com>
2023-09-26 04:07:21 -0700

    export in json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Returns informations about an employee in a csv file"""
+import json
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    users_url = '{}users/'.format(url)
+    res = requests.get(users_url)
+    users = res.json()
+    users_fil = {}
+
+    for user in users:
+        user_id = user.get('id')
+        user_name = user.get('username')
+        url_todo = '{}todos?userId={}'.format(url, user_id)
+        res = requests.get(url_todo)
+        tasks = res.json()
+        list_tasks = []
+        for task in tasks:
+            fil = {}
+            fil['username'] = user_name
+            fil['task'] = task.get('title')
+            fil['completed']: task.get('completed')
+            list_tasks.append(fil)
+
+        users_fil[str(user_id)] = list_tasks
+
+    user_file = 'todo_all_employees.json'
+    with open(user_file, mode='w') as file_json:
+        json.dump(users_fil, file_json)
+
+    print('Json file "{}" has been successfully exported.'.format(user_file))
