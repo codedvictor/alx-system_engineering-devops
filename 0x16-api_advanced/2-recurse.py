3a6d4d005c3cfaee27e3be14494a5d8f94798464
Ige Victor Kolade <victorkoladeige@gmail.com>
2023-10-10 15:24:56 -0700

    fix bug

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ a recursive function that queries the Reddit API and returns a list
-containing the titles of all hot articles for a given subreddit. If no results
-are found for the given subreddit, the function should return None."""
+containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit, the function should
+return None."""
 
 from requests import get
 after = None
