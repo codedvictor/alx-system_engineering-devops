3e8cfb055d3af5459ab51f8359ecb6f6382cfcd2
Ige Victor Kolade <victorkoladeige@gmail.com>
2023-10-11 09:36:09 -0700

    puppet code

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Find the issue, fix it and
+# then automate it using Puppet
+
+exec { 'fix-wordpress':
+  command => "/bin/sed -i /var/www/html/wp-settings.php \
+  -e 's/class-wp-locale.phpp/class-wp-locale.php/'"
+}
